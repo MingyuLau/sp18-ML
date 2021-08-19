31e27f53faeaaedc25f1f0b410777c3be13ad63f
MingyuLau <lmy20011207@gmail.com>
2021-08-19 11:44:42 +0800

    add

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class ArrayDeque {
+public class ArrayDeque<Item> {
+
+
+        private Item[] items;
+        private int size;
+
+        /** Creates an empty list. */
+        public ArrayDeque() {
+            items = (Item[]) new Object[100];
+            size = 0;
+        }
+
+        /** Resizes the underlying array to the target capacity. */
+        private void resize(int capacity) {
+            Item[] a = (Item[]) new Object[capacity];
+            System.arraycopy(items, 0, a, 0, size);
+            items = a;
+        }
+
+        /** Inserts X into the back of the list. */
+        public void addLast(Item x) {
+            if (size == items.length) {
+                resize(size + 1);
+            }
+
+            items[size] = x;
+            size = size + 1;
+        }
+
+        /** Returns the item from the back of the list. */
+        public Item getLast() {
+            return items[size - 1];
+        }
+        /** Gets the ith item in the list (0 is the front). */
+        public Item get(int i) {
+            return items[i];
+        }
+
+        /** Returns the number of items in the list. */
+        public int size() {
+            return size;
+        }
+
+        /** Deletes item from back of the list and
+         * returns deleted item. */
+        public Item removeLast() {
+            Item x = getLast();
+            items[size - 1] = null;
+            size = size - 1;
+            return x;
+        }
+
+
+
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+public class LinkedListDeque<T>{
+
+    public class TNode{
+        public TNode prev;
+        public T value;
+        public TNode next;
+
+        public TNode(TNode p, T x, TNode n){
+            prev = p;
+            value = x;
+            next = n;
+        }
+    }
+
+    private int size = 0;
+    private TNode last;
+    private TNode sentinel;
+
+    public LinkedListDeque(){
+        sentinel = new TNode(null,null,null);
+        last = sentinel;
+        sentinel.prev = last;
+        sentinel.next = sentinel;
+    }
+    public void addFirst(T item){
+        sentinel.next = new TNode(sentinel, item, sentinel);
+        last = sentinel.next;
+        size += 1;
+    }
+
+    public void addLast(T item){
+        last.next = new TNode(last, item, sentinel);
+        last = last.next;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        if(size == 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int size(){return size;}
+
+    public void printDeque(){
+        TNode pointer = sentinel;
+        int cnt;
+        for(cnt = 0; cnt < size; cnt++){
+            pointer = pointer.next;
+            System.out.print(pointer.value + " ");
+
+        }
+    }
+
+    public T removeFirst(){
+        if(size == 0){
+            return null;
+        }else{
+            sentinel.next = sentinel.next.next;
+            sentinel = sentinel.next.prev;
+            size -= 1;
+            return sentinel.next.value;
+        }
+    }
+
 }
