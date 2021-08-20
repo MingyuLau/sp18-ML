8d9cbda09aa520f209264b5fd2fc5171983cec29
MingyuLau <lmy20011207@gmail.com>
2021-08-20 15:14:57 +0800

    copy

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,55 +1,151 @@
-public class ArrayDeque<Item> {
+/**
+ * Deque implemented by array.
+ */
+public class ArrayDeque<T> {
 
+    private T[] items;
+    private int left;
+    private int right;
+    private int capacity = 8;
 
-        private Item[] items;
-        private int size;
+    public ArrayDeque() {
+        items = (T[]) new Object[capacity];
+        left = right = 0;
+    }
 
-        /** Creates an empty list. */
-        public ArrayDeque() {
-            items = (Item[]) new Object[100];
-            size = 0;
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(T item) {
+        if (isFull()) {
+            resize((int) (capacity * 1.5));
         }
+        left = (left - 1 + capacity) % capacity;
+        items[left] = item;
+    }
 
-        /** Resizes the underlying array to the target capacity. */
-        private void resize(int capacity) {
-            Item[] a = (Item[]) new Object[capacity];
-            System.arraycopy(items, 0, a, 0, size);
-            items = a;
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(T item) {
+        if (isFull()) {
+            resize((int) (capacity * 1.5));
         }
+        items[right] = item;
+        right = (right + 1 + capacity) % capacity;
+    }
 
-        /** Inserts X into the back of the list. */
-        public void addLast(Item x) {
-            if (size == items.length) {
-                resize(size + 1);
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty() {
+        return left == right;
+    }
+
+    /** Returns the number of items in the deque. */
+    public int size() {
+        return (right - left + capacity) % capacity;
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque() {
+        if (left < right) {
+            for (int i = left; i < right; i++) {
+                if (i == right - 1) {
+                    System.out.println(items[i]);
+                    break;
+                }
+                System.out.print(items[i] + " ");
             }
-
-            items[size] = x;
-            size = size + 1;
+        } else if (left > right) {
+            for (int i = left; i < capacity; i++) {
+                System.out.print(items[i] + " ");
+            }
+            for (int i = 0; i < right; i++) {
+                if (i == right - 1) {
+                    System.out.println(items[i]);
+                    break;
+                }
+                System.out.print(items[i] + " ");
+            }
         }
+    }
 
-        /** Returns the item from the back of the list. */
-        public Item getLast() {
-            return items[size - 1];
+    /**
+     * Removes and returns the item at the front of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
         }
-        /** Gets the ith item in the list (0 is the front). */
-        public Item get(int i) {
-            return items[i];
+        T res = items[left];
+        left = (left + 1) % capacity;
+        if (isLowUsageRate()) {
+            resize((int) (capacity * 0.5));
         }
+        return res;
+    }
 
-        /** Returns the number of items in the list. */
-        public int size() {
-            return size;
+    /**
+     * Removes and returns the item at the back of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
         }
-
-        /** Deletes item from back of the list and
-         * returns deleted item. */
-        public Item removeLast() {
-            Item x = getLast();
-            items[size - 1] = null;
-            size = size - 1;
-            return x;
+        right = (right - 1 + capacity) % capacity;
+        T res = items[right];
+        if (isLowUsageRate()) {
+            resize((int) (capacity * 0.5));
         }
+        return res;
+    }
 
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item,
+     * and so forth. If no such item exists, returns null. Must not alter the deque!
+     */
+    public T get(int index) {
+        if (index < 0 || index >= size() || isEmpty()) {
+            return null;
+        }
+        if (left < right) {
+            return items[index + left];
+        } else if (left > right) {
+            if (index + left < capacity) {
+                return items[index + left];
+            } else {
+                return items[(index + left) % capacity];
+            }
+        }
+        return null;
+    }
 
+    private boolean isFull() {
+        return size() == capacity - 1;
+    }
+
+    private boolean isLowUsageRate() {
+        return capacity >= 16 && size() / (double) capacity < 0.25;
+    }
+
+    private void resize(int newSize) {
+        T[] newArray = (T[]) new Object[newSize];
+
+        int size = size();
+        if (left < right) {
+            for (int i = left, j = 0; i < right && j < size; i++, j++) {
+                newArray[j] = items[i];
+            }
+        } else if (left > right) {
+            int j = 0;
+            for (int i = left; j < capacity - left; i++, j++) {
+                newArray[j] = items[i];
+            }
+            for (int i = 0; j < size; i++, j++) {
+                newArray[j] = items[i];
+            }
+        }
+        left = 0;
+        right = size;
+        items = newArray;
+        capacity = newSize;
+    }
 
 }
